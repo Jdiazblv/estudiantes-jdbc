f030b5caa6f11205b6a1afda033e8b5ad685e463
Jorge Esteban Diaz Bolivar <jorgediazblv@gmail.com>
2026-02-28 20:48:33 -0500

    Conexion JDBC y CRUD implementado

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,52 @@ import java.sql.*;
 public class Main {
     public static void main(String[] args) {
         // Declarar las Variables de conexión
-
+        String usuario="root";
+        String contra="1234";
+        String url="jdbc:mysql://localhost:3306/universidad";
+         // 1. Load Driver (Optional for newer JDBC versions)
+        Connection cnx;
+        Statement st;
+        ResultSet rs;
 
         // 1. Load Driver (Optional for newer JDBC versions)
-
-
         try { // Manejo de Errores Try catch
             // 2. Establecer la conexión e Imprimir
-
+            cnx = DriverManager.getConnection(url, usuario, contra);
+            System.out.println("Conectado correctamente.");
             // 3. Create Statement
-
+            st=cnx.createStatement();
             // 4. Execute Query
             //CRUD - INSERT - SELECT - UPDATE - DELETE
-
+            // INSERT
+            String sqlInsert = ("INSERT INTO estudiantes (nombre, correo) VALUES ('Pepito', 'pepito@gmail.com')");
+            st.executeUpdate (sqlInsert);
+            System.out.println("Estudiante ingresado correctamente.");
+            // UPDATE
+            st.executeUpdate("UPDATE estudiantes SET nombre='NombreActualizado' WHERE id=3");
+            System.out.println("Estudiante actualizado.");
+            // DELET
+            st.executeUpdate("DELETE FROM estudiantes WHERE id=3");
+            System.out.println("Estudiante eliminado.");
+            // SELECT
+            rs = st.executeQuery("SELECT * FROM estudiantes");
             // 5. Process Results
-            //do {
-            //    System.out.println(rs.getInt("id")+" : "+ rs.getString("nombre")+ " "+ rs.getString("correo"));
-            //}while (rs.next());
+            if(rs.next()){
+                do{
+                    System.out.println(
+                        rs.getInt("id")+" : "+
+                        rs.getString("nombre")+ " - "+
+                        rs.getString("correo"));
+                }while(rs.next());
+            }
             // 6. Close resources
             // Cerrar el Resultado de la Consulta
+            rs.close();
             // Cerra el statement
+            st.close();
             // Cerrar la conexion a la base de datos e Imprimir
+            cnx.close();
+            System.out.println("Conexion cerrada.");
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
